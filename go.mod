65823f58697f9d904bf7dfa14865656bb6feed45
Cooper Lemley <cooperlemley@Coopers-MacBook-Air.local>
2025-05-15 09:24:31 -0400

    added cpu information

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/COOPERLEMLEY02/go_sys_fetch
 
 go 1.24.3
+
+require (
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	. "github.com/klauspost/cpuid/v2"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+
+	CPUBrand, PhysicalCores, ThreadsPerCore := cpuInfo()
+
+	fmt.Println("\033[31mName:\033[0m", CPUBrand)
+	fmt.Println("\033[31mPhysical Cores:\033[0m", PhysicalCores)
+	fmt.Println("\033[31mThreads Per Core:\033[0m", ThreadsPerCore)
+
+}
+
+func cpuInfo() (string, int, int) {
+
+	CPUBrand := CPU.BrandName
+	PhysicalCores := CPU.PhysicalCores
+	ThreadsPerCore := CPU.ThreadsPerCore
+
+	return CPUBrand, PhysicalCores, ThreadsPerCore
+
 }
